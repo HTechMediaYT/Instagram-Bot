8ef2ec2dd9bad5f31df95157d90c4bfeb602d59f
HTechMedia <85754896+HTechMediaYT@users.noreply.github.com>
2021-08-26 15:59:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,7 +63,7 @@ def username(update, context):
     try:
         user = Profile.from_username(L.context, query)
         caption_msg = f'''📛*Name*📛: {user.full_name} \n😁*Followers*😁: {user.followers} \n🤩*Following*🤩: {user.followees}\
-         \n🧐*Account Type*🧐: {acc_type(user.is_private)} \nThank You For Using The bot 😀😀 \n\n<b>@NxtStark</b>'''
+         \n🧐*Account Type*🧐: {acc_type(user.is_private)} \nThank You For Using The bot 😀😀 \n\n*@NxtStark❤*'''
         context.bot.send_photo(
             chat_id=chat_id, photo=user.profile_pic_url,
             caption=caption_msg, parse_mode='MARKDOWN')
